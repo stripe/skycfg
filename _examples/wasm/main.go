f62bbbf9f13ee5cb51d433902a5816e7694fe367
jmillikin-stripe <jmillikin@stripe.com>
2018-11-26 15:00:37 -0800

    Extend the wasm demo to use the upstream Address Book example. (#12)
    https://developers.google.com/protocol-buffers/docs/gotutorial
    
    This is easier for users to get started with than the full Kubernetes or
    Envoy APIs, and as a nice side-benefit reduces the size of the compiled
    WASM by ~60%.
    
    The demo page itself is also extended to support multiple output
    formats (YAML, JSON, and Text Protobuf).

--- _examples/wasm/main.go
+++ _examples/wasm/main.go
@@ -1,24 +1,16 @@
 package main
 
 import (
-	"bytes"
 	"context"
 	"fmt"
-	"reflect"
 	"syscall/js"
 
-	gogo_proto "github.com/gogo/protobuf/proto"
 	"github.com/golang/protobuf/jsonpb"
 	"github.com/golang/protobuf/proto"
 	yaml "gopkg.in/yaml.v2"
 
-	_ "github.com/envoyproxy/go-control-plane/envoy/api/v2"
-	_ "k8s.io/api/apps/v1"
-	_ "k8s.io/api/batch/v1"
-	_ "k8s.io/api/core/v1"
-	_ "k8s.io/api/storage/v1"
-
 	"github.com/stripe/skycfg"
+	_ "github.com/stripe/skycfg/_examples/wasm/addressbook"
 )
 
 type stubFileReader struct {
@@ -36,74 +28,66 @@ func (r *stubFileReader) ReadFile(ctx context.Context, path string) ([]byte, err
 	return []byte(r.content), nil
 }
 
-type protoRegistry struct{}
+func runDemo(content string) ([]js.Value, error) {
+	config, err := skycfg.Load(
+		context.Background(),
+		"<stdin>",
+		skycfg.WithFileReader(&stubFileReader{content}),
+	)
+	if err != nil {
+		return nil, err
+	}
 
-func (*protoRegistry) UnstableProtoMessageType(name string) (reflect.Type, error) {
-	if t := proto.MessageType(name); t != nil {
-		return t, nil
+	messages, err := config.Main(context.Background())
+	if err != nil {
+		return nil, err
 	}
-	if t := gogo_proto.MessageType(name); t != nil {
-		return t, nil
-	}
-	return nil, nil
-}
 
-func (*protoRegistry) UnstableEnumValueMap(name string) map[string]int32 {
-	if ev := proto.EnumValueMap(name); ev != nil {
-		return ev
+	json := &jsonpb.Marshaler{
+		OrigName:     true,
+		Indent:       "  ",
+		EmitDefaults: true,
 	}
-	if ev := gogo_proto.EnumValueMap(name); ev != nil {
-		return ev
-	}
-	return nil
-}
 
-func messagesToYaml(msgs []proto.Message) (string, error) {
-	var buf bytes.Buffer
-	var jsonMarshaler = &jsonpb.Marshaler{OrigName: true}
-	for _, msg := range msgs {
-		jsonData, err := jsonMarshaler.MarshalToString(msg)
+	var out []js.Value
+	for _, msg := range messages {
+		jsonData, err := json.MarshalToString(msg)
 		if err != nil {
-			return "", fmt.Errorf("json.Marshal: %v", err)
+			return nil, fmt.Errorf("json.Marshal: %v", err)
 		}
 		var yamlMap yaml.MapSlice
 		if err := yaml.Unmarshal([]byte(jsonData), &yamlMap); err != nil {
-			return "", fmt.Errorf("yaml.Unmarshal: %v", err)
+			return nil, fmt.Errorf("yaml.Unmarshal: %v", err)
 		}
 		yamlData, err := yaml.Marshal(yamlMap)
 		if err != nil {
-			return "", fmt.Errorf("yaml.Marshal: %v", err)
+			return nil, fmt.Errorf("yaml.Marshal: %v", err)
 		}
-		fmt.Fprintf(&buf, "---\n%s", string(yamlData))
+		out = append(out, js.ValueOf(map[string]interface{}{
+			"yaml":  string(yamlData),
+			"json":  jsonData,
+			"proto": proto.MarshalTextString(msg),
+		}))
 	}
-	return buf.String(), nil
+	return out, nil
+}
+
+func jsMain(args []js.Value) {
+	content := args[0].String()
+	result, err := runDemo(content)
+	if err != nil {
+		args[1].Call("err", err.Error())
+		return
+	}
+	var out []interface{}
+	for _, item := range result {
+		out = append(out, js.ValueOf(item))
+	}
+	args[1].Call("ok", out)
 }
 
 func main() {
-	toYaml := func(args []js.Value) {
-		content := args[0].String()
-		config, err := skycfg.Load(
-			context.Background(),
-			"<stdin>",
-			skycfg.WithProtoRegistry(&protoRegistry{}),
-			skycfg.WithFileReader(&stubFileReader{content}))
-		if err != nil {
-			args[1].Call("err", err.Error())
-			return
-		}
-		messages, err := config.Main(context.Background())
-		if err != nil {
-			args[1].Call("err", err.Error())
-			return
-		}
-		yaml, err := messagesToYaml(messages)
-		if err != nil {
-			args[1].Call("err", err.Error())
-			return
-		}
-		args[1].Call("ok", yaml)
-	}
-	js.Global().Set("skycfg_to_yaml", js.NewCallback(toYaml))
+	js.Global().Set("skycfg_main", js.NewCallback(jsMain))
 	c := make(chan struct{}, 0)
 	<-c
 }
