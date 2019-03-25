ff7f543b9a99af8d85564bd385817dfd722a9e4d
Charles Xu <charlesxu1995@gmail.com>
2019-03-25 16:37:16 -0700

    [yaml]: support yaml.unmarshal (#47)

--- internal/go/skycfg/yaml.go
+++ internal/go/skycfg/yaml.go
@@ -18,6 +18,8 @@ package skycfg
 
 import (
 	"bytes"
+	"fmt"
+	"reflect"
 
 	"go.starlark.net/starlark"
 	yaml "gopkg.in/yaml.v2"
@@ -28,7 +30,8 @@ func YamlModule() starlark.Value {
 	return &Module{
 		Name: "yaml",
 		Attrs: starlark.StringDict{
-			"marshal": yamlMarshal(),
+			"marshal":   yamlMarshal(),
+			"unmarshal": yamlUnmarshal(),
 		},
 	}
 }
@@ -59,4 +62,58 @@ func fnYamlMarshal(t *starlark.Thread, fn *starlark.Builtin, args starlark.Tuple
 		return nil, err
 	}
 	return starlark.String(yamlBytes), nil
+}
+
+// yamlUnmarshal returns a Starlark function for unmarshaling yaml content to
+// to starlark values.
+//
+// def yaml.unmarshal(yaml_content) -> (dicts, lists, etc)
+func yamlUnmarshal() starlark.Callable {
+	return starlark.NewBuiltin("yaml.unmarshal", fnYamlUnmarshal)
+}
+
+func fnYamlUnmarshal(t *starlark.Thread, fn *starlark.Builtin, args starlark.Tuple, kwargs []starlark.Tuple) (starlark.Value, error) {
+	var blob string
+	if err := starlark.UnpackPositionalArgs(fn.Name(), args, nil, 1, &blob); err != nil {
+		return nil, err
+	}
+	var inflated interface{}
+	if err := yaml.Unmarshal([]byte(blob), &inflated); err != nil {
+		return nil, err
+	}
+	return toStarlarkValue(inflated)
+}
+
+// toStarlarkValue is a DFS walk to translate the DAG from go to starlark
+func toStarlarkValue(obj interface{}) (starlark.Value, error) {
+	rt := reflect.TypeOf(obj)
+	switch rt.Kind() {
+	case reflect.String:
+		return starlark.String(obj.(string)), nil
+	case reflect.Map:
+		ret := &starlark.Dict{}
+		for k, v := range obj.(map[interface{}]interface{}) {
+			starval, err := toStarlarkValue(v)
+			if err != nil {
+				return nil, err
+			}
+			if err = ret.SetKey(starlark.String(k.(string)), starval); err != nil {
+				return nil, err
+			}
+		}
+		return ret, nil
+	case reflect.Slice:
+		slice := obj.([]interface{})
+		starvals := make([]starlark.Value, len(slice))
+		for i, element := range slice {
+			v, err := toStarlarkValue(element)
+			if err != nil {
+				return nil, err
+			}
+			starvals[i] = v
+		}
+		return starlark.NewList(starvals), nil
+	default:
+		return nil, fmt.Errorf("%v is not a slice, map, or string", obj)
+	}
 }
