1239f1f95d8e4a777dedc5aa2ad30d750c8ddcb2
Isaac Diamond <idiamond@stripe.com>
2020-09-26 19:04:52 -0700

    refactor

--- _examples/envoy/run.sh
+++ _examples/envoy/run.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-exec docker run -p10000:10000 -p9901:9901  -v`pwd`:/etc/envoy -it envoyproxy/envoy:v1.14-latest
+trap "kill 0" EXIT
+
+python3 -m http.server -d www 2>/dev/null &
+docker run -i -p10000:10000 -p9901:9901  -v`pwd`:/etc/envoy -it envoyproxy/envoy:v1.14-latest
