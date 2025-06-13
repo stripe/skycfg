5de623b88f157ede85f929cc24f08ab5575b768b
Timothy Gu <timothyg@stripe.com>
2025-06-13 09:30:30 -0700

    Add FSFileReader (#139)
    Add an implementation of [FileReader] that reads from a [io/fs.FS]. This is a version of what we've been using internally.
    
    [io/fs.FS]: https://pkg.go.dev/io/fs#FS
    [FileReader]: https://pkg.go.dev/github.com/stripe/skycfg#FileReader

--- skycfg.go
+++ skycfg.go
@@ -22,9 +22,7 @@ import (
 	"context"
 	"fmt"
 	"io"
-	"io/ioutil"
 	"os"
-	"path"
 	"path/filepath"
 	"strings"
 	"sync"
@@ -50,48 +48,6 @@ const (
 	contextKey   = "context"   // has type context.Context
 	logOutputKey = "logoutput" // has type io.Writer
 )
-
-// A FileReader controls how load() calls resolve and read other modules.
-type FileReader interface {
-	// Resolve parses the "name" part of load("name", "symbol") to a path. This
-	// is not required to correspond to a true path on the filesystem, but should
-	// be "absolute" within the semantics of this FileReader.
-	//
-	// fromPath will be empty when loading the root module passed to Load().
-	Resolve(ctx context.Context, name, fromPath string) (path string, err error)
-
-	// ReadFile reads the content of the file at the given path, which was
-	// returned from Resolve().
-	ReadFile(ctx context.Context, path string) ([]byte, error)
-}
-
-type localFileReader struct {
-	root string
-}
-
-// LocalFileReader returns a FileReader that resolves and loads files from
-// within a given filesystem directory.
-func LocalFileReader(root string) FileReader {
-	if root == "" {
-		panic("LocalFileReader: empty root path")
-	}
-	return &localFileReader{root}
-}
-
-func (r *localFileReader) Resolve(ctx context.Context, name, fromPath string) (string, error) {
-	if fromPath == "" {
-		return name, nil
-	}
-	if filepath.Separator != '/' && strings.ContainsRune(name, filepath.Separator) {
-		return "", fmt.Errorf("load(%q): invalid character in module name", name)
-	}
-	resolved := filepath.Join(r.root, filepath.FromSlash(path.Clean("/"+name)))
-	return resolved, nil
-}
-
-func (r *localFileReader) ReadFile(ctx context.Context, path string) ([]byte, error) {
-	return ioutil.ReadFile(path)
-}
 
 // NewProtoMessage returns a Starlark value representing the given Protobuf
 // message. It can be returned back to a proto.Message() via AsProtoMessage().
